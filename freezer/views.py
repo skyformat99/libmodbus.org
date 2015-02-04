f32fe057edc585332c2b8080c1079806116c6498
Stéphane Raimbault <stephane.raimbault@gmail.com>
2015-02-04 20:29:00 +0100

    Views and style of libmodbus.org

--- freezer/views.py
+++ freezer/views.py
@@ -8,19 +8,34 @@ from flask import render_template, send_from_directory, abort
 from .app import app
 from .models import Article
 
+# Allows trailing slashes (avoid 404)
+app.url_map.strict_slashes = False
 
 def by_date(articles):
     return sorted(articles, reverse=True, key=lambda p: p['published'])
 
 
-@app.route('/')
-def home():
-    return render_template('home.html', articles=by_date(Article.get_posts()))
+@app.route('/', endpoint='home')
+@app.route('/news')
+def news():
+    return render_template(
+        'news.html', selected='news',
+        articles=by_date(Article.get_posts()))
 
 
-@app.route('/about/')
-def about():
-    return render_template('article.html', article=Article.load('', 'about'))
+@app.route('/documentation')
+def documentation():
+    return render_template('documentation.html', selected='documentation')
+
+
+@app.route('/download')
+def download():
+    return render_template('download.html', selected='download')
+
+
+@app.route('/reportbug')
+def reportbug():
+    return render_template('reportbug.html', selected='reportbug')
 
 
 @app.route('/tags/')
@@ -30,15 +45,17 @@ def tags():
         for tag in article.meta.get('tags', []):
             counts[tag] = counts.get(tag, 0) + 1
 
-    return render_template('tag_list.html', tags=[
-        # count => weight: 1 => 100, 10 => 150, 100 => 200
-        (tag, int(100 + 50 * math.log10(count)))
-        # sorted alphabetically by tag name
-        for tag, count in sorted(counts.items())
-    ])
+    return render_template(
+        'tag_list.html', selected='news',
+        tags=[
+            # count => weight: 1 => 100, 10 => 150, 100 => 200
+            (tag, int(100 + 50 * math.log10(count)))
+            # sorted alphabetically by tag name
+            for tag, count in sorted(counts.items())
+        ])
 
 
-@app.route('/tags/<name>/')
+@app.route('/tags/<name>')
 def tag(name):
     articles = by_date(
         a for a in Article.get_posts() if name in a.meta.get('tags', [])
@@ -46,18 +63,20 @@ def tag(name):
     if not articles:
         abort(404)
 
-    return render_template('tag.html', tag=name, articles=articles)
+    return render_template('tag.html', selected='news', tag=name, articles=articles)
 
 
 @app.route('/<int:year>/')
 def archives(year):
     articles = by_date(Article.get_posts(year=str(year)))
-    return render_template('archives.html', articles=articles, year=year)
+    return render_template(
+        'archives.html', selected='news', articles=articles, year=year)
 
 
 @app.route('/<int:year>/<name>/')
 def article(year, name):
-    return render_template('article.html', article=Article.load(str(year), name))
+    return render_template(
+        'article.html', selected='news', article=Article.load(str(year), name))
 
 
 @app.route('/<int:year>/<name>/<path:path>')
